1e8e782af5d23f764ab955a0a344d09464145e15
GaryTimeless <gary.timeless@gmx.de>
2023-11-01 12:22:19 +0100

    new Build test

---
+++ assets/keyboard-10f2bd47.js
@@ -0,0 +1,3 @@
+import{o as t}from"./index-c168939f.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */var n;(function(e){e.Unimplemented="UNIMPLEMENTED",e.Unavailable="UNAVAILABLE"})(n||(n={}));var o;(function(e){e.Body="body",e.Ionic="ionic",e.Native="native",e.None="none"})(o||(o={}));const a={getEngine(){const e=t();if(e!=null&&e.isPluginAvailable("Keyboard"))return e.Plugins.Keyboard},getResizeMode(){const e=this.getEngine();return e!=null&&e.getResizeMode?e.getResizeMode().catch(i=>{if(i.code!==n.Unimplemented)throw i}):Promise.resolve(void 0)}};export{a as K,o as a};
