84ce8c786625eeaf8134f375cfd9e652c2cd2a96
GaryTimeless <gary.timeless@gmx.de>
2025-04-10 23:07:17 +0200

    Deploying latest build

--- assets/ios.transition-legacy-8984430c.js
+++ assets/ios.transition-legacy-8984430c.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6607e4df.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.b,n=t.g}],execute:function(){
+System.register(["./index-legacy-ddffd035.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.b,n=t.g}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-be5a825d.js
+++ assets/status-tap-legacy-be5a825d.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6607e4df.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.e,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-ddffd035.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.e,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
