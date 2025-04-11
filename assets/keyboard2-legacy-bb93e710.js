78010ac6c5dee934cb0a4b9e07c48a4121cfbbcf
GaryTimeless <gary.timeless@gmx.de>
2025-04-11 23:42:07 +0200

    Deploying latest build

--- assets/keyboard2-legacy-bb93e710.js
+++ assets/keyboard2-legacy-bb93e710.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ddffd035.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-c14b5198.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-b4e5f840.js
+++ assets/md.transition-legacy-b4e5f840.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ddffd035.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.g,i=e.b}],execute:function(){
+System.register(["./index-legacy-c14b5198.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.g,i=e.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
